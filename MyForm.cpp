f08b7eec1bb7f49cc5e3a3f1bef933ff10ffded6
OsamaAhmedSeif <111289552+OsamaAhmedSeif@users.noreply.github.com>
2022-08-20 23:13:47 +0200

    Add files via upload

---
+++ MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+
+
+void main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	project5::MyForm frm;
+	Application::Run(% frm);
+
+}
+
